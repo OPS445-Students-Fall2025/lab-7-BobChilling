2081977929b3fc20ac204120979c7fdce3179533
Wayson Cao <wcao23@myseneca.ca>
2025-11-14 18:45:54 -0500

    lab7

--- lab7e.py
+++ lab7e.py
@@ -45,6 +45,15 @@ class Time:
         if self.minute >= 60 or self.second >= 60 or self.hour >= 24:
             return False
         return True
+    
+    def __str__(self):
+        '''return a string representation for the object self'''
+        return  f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        '''return a string representation for the object self'''
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
 
 def sec_to_time(seconds):
     '''convert a given number of seconds to a time object in 
